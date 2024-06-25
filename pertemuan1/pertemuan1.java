f97c6486fca09618d9305692521768aad1d5a76b
Anjasputraa <115926820+Anjasputraa@users.noreply.github.com>
2024-06-25 14:18:04 +0800

    Add files via upload

---
+++ pertemuan1/pertemuan1.java
@@ -0,0 +1,22 @@
+package pertemuan1;
+
+/**
+ *
+ * @author Komang Agus Anjas Putra
+ * NIM : 2201010760
+ * Tanggal : 18 maret 2024
+ */
+public class pertemuan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama = "Komang Agus Anjas Putra";
+        String nim = "2201010760";
+        
+        System.out.println("nama    :"+nama);
+        System.out.println("Nim     :"+nim);
+    }
+    
+}
