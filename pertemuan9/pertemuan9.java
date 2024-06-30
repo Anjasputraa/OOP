802e365563091ed1d17e2c8ae8490d95363d7949
Anjasputraa <115926820+Anjasputraa@users.noreply.github.com>
2024-06-30 15:52:04 +0800

    Add files via upload

---
+++ pertemuan9/pertemuan9.java
@@ -0,0 +1,18 @@
+package pertemuan9;
+
+/**
+ *
+ * @author Komang Agus Anjas Putra
+ * nim : 2201010760
+ * tanggal : 13 mei 2024
+ */
+public class pertemuan9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new frame01().setVisible(true);
+    }
+    
+}
