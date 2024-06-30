e6683068d2a6e4e542cec6ff9160adecb1eb9dff
Anjasputraa <115926820+Anjasputraa@users.noreply.github.com>
2024-06-30 15:46:32 +0800

    Add files via upload

---
+++ pertemuan1201/Pertemuan1201.java
@@ -0,0 +1,21 @@
+package pertemuan1201;
+
+/**
+ *
+ * Komang Agus Anjas Putra
+ * nim: 2201010760
+ * tanggal: 6 mei 2024
+ */
+public class Pertemuan1201 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //new jform1().setVisible(true);
+        jform1 A1 = new jform1();
+        A1.setResizable(false);
+        A1.setVisible(true);
+    }
+    
+}
